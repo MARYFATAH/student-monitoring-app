7b20645a4319637cee7474685b48d233cda55b1e
Ralf Siewert <ralf@actyvy.st>
2025-04-04 08:28:49 +0200

    wip

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -4,7 +4,5 @@ import { getUsers } from "../handlers/users.js";
 const router = express.Router();
 
 router.get("/", getUsers);
-router.post("/:id/students", addStudentToCourse);
-router.delete("/:id/students/:studentId", removeStudentFromCourse);
 
 export default router;
